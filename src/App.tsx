702b20fde3d360c1ba80faa32b03c49e9a2fb3ff
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-15 20:23:46 -0500

    style(app): apply style guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path= "/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
