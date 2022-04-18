8a7bfff27dfb847a224b27891e08cccc3d87013a
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-18 18:57:30 -0500

    feat(footer): add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 text-center lg:text-left px-6 pt-6">
+      <div className="text-gray-700 text-center md:mr-auto mb-6">
+        <a
+          className="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
+          href="mailto:fcojavierlpz91@gmail.com"
+        >
+          Contact me
+        </a>
+      </div>
+      <div className="text-gray-700 text-center p-4">
+        © 2021 Copyright fcojavierlpz
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
