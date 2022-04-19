58135142dea6975a2669d950a90683f27744678d
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-18 19:01:43 -0500

    refactor(app): add footer and navbar for the main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <div className="flex flex-col h-screen justify-between">
+        <NavBar />
+        <main className="mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   )
