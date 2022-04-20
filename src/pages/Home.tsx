dc31a70a1f258e72a57545272e05fab347ccc98e
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-19 19:40:47 -0500

    refactor(home): import ratessummary component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
-const home = () => <div>Welcome to Home!</div>
+import RatesSummary from '../components/RateSummary'
+
+const home = () => (
+  <div className="grid place-items-center h-96">
+    <RatesSummary />
+  </div>
+)
 
 export default home
