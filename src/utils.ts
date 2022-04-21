59307d0913389f8db69b47ca67f7e3de93bcedaa
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-21 16:24:28 -0500

    refactor(utils): add function for get last months by given param

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,18 @@ const groupBy = (arr: object[]) =>
     return acc
   }, {})
 
+const getLastMonthsBy = (monthNumber: number): string[] => {
+  const today = new Date()
+  const lastMonths = []
+  for (let i = 0; i < monthNumber; i++) {
+    const month = today.getMonth() - i
+    const year = today.getFullYear()
+    const date = new Date(year, month, 0)
+    lastMonths.push(date.toISOString().split('T')[0])
+  }
+  return lastMonths
+}
+
 const join = (arr: string[], separator = ','): string => arr.join(separator)
 
-export { groupBy, join }
+export { groupBy, getLastMonthsBy, join }
