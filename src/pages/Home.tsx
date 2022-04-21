ed7773d4c6e48fc9777ef1ce59bac2e2dfbe17bb
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-21 18:29:14 -0500

    refactor(home): make responsive a line chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import RatesSummary from '../components/RateSummary'
+import styles from '../styles/home.module.css'
 
 const home = () => (
-  <div className="grid place-items-center h-96">
+  <div className={styles.home}>
     <RatesSummary />
   </div>
 )
